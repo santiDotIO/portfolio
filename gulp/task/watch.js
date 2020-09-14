b64e7e98ef69e095baca1d0643c708d08793d400
Santiago J Sosa <me@santisosa.com>
2020-09-14 19:27:56 -0400

    [v2] Remove gulp

--- gulp/task/watch.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const path = require('../helpers/paths.js').watch;
-
-const gulp = require('gulp');
-
-module.exports = () => {
-    gulp.watch(path.scss, ['scss']);
-    gulp.watch(path.js, ['browserify']);
-}
