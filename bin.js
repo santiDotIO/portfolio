89f7f8df879e1ec6c9bbfbf0f5e932fd94128cd6
Santiago J Sosa <me@santisosa.com>
2020-09-17 23:50:25 -0400

    [v2] Copy only necessary fonts

--- bin.js
+++ bin.js
@@ -1,13 +1,23 @@
 #!/usr/bin/env node
 const args = process.argv.slice(2);
+const FileCopy = require('./bin/copy');
 const HandleBarsCompiler = require('./bin/hbs');
-const { exec } = require("child_process");
 
 
 // console.log(tailwindcss)
 switch (args[0]) {
     case 'hbs':
-        const hbs = new HandleBarsCompiler();
-        hbs.compile();
+        (new HandleBarsCompiler()).compile();
+    break;
+    case 'copy':
+        const copyInstace = new FileCopy([
+            'montserrat_alternates/MontserratAlternates-Light.ttf',
+            'montserrat_alternates/MontserratAlternates-Medium.ttf',
+            'montserrat_alternates/MontserratAlternates-Black.ttf',
+        ]);
+        
+        copyInstace.src = '/src/assets/';
+        copyInstace.dest = '/public/assets/fonts/';
+        copyInstace.copy();
     break;
 }
